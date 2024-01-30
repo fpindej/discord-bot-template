6457fda96e5ab40615135fd3e61fbd331a5a57fd
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-01-30 13:59:26 +0100

    Add appsettings per environment to generic host

--- src/DiscordTemplateBot.Console/Program.cs
+++ src/DiscordTemplateBot.Console/Program.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+        config.AddJsonFile("appsettings.json", false, true);
+        config.AddJsonFile($"appsettings.{environmentName}.json", true, true);
+    })
     .Build();
 
 await host.RunAsync();
