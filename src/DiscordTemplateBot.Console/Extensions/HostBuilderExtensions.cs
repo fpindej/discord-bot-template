b5fba161c17f48768302a4622d59a5fb90c8028f
Filip Dorian Pindej <24482879+fpindej@users.noreply.github.com>
2024-01-30 14:20:15 +0100

    Introduce Serilog (#2)
    * Create logging assembly
    
    * Install Serilog
    
    * Implement logger configuration setup
    
    * Configure Serilog logging in generic host
    
    * Add Serilog configuration to appsettings
    
    ---------
    
    Co-authored-by: Filip Dorian Pindej <fpindej@users.noreply.github.com>

---
+++ src/DiscordTemplateBot.Console/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DiscordTemplateBot.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace DiscordTemplateBot.Console.Extensions;
+
+internal static class HostBuilderExtensions
+{
+    public static IHostBuilder ConfigureSerilog(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureLogging((builder, logging) =>
+        {
+            logging.ClearProviders();
+            LoggerConfigurationHelper.SetupLoggerConfiguration(builder.Configuration);
+        }).UseSerilog();
+
+        return hostBuilder;
+    }
+}
