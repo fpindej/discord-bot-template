8a3c34a97728ee66c254137d835fc0aaee324953
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-01-30 14:17:44 +0100

    Configure Serilog logging in generic host

--- src/DiscordTemplateBot.Console/Program.cs
+++ src/DiscordTemplateBot.Console/Program.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DiscordTemplateBot.Console.Extensions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureSerilog()
     .ConfigureAppConfiguration(config =>
     {
         var environmentName = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
