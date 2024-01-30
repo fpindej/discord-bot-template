a558dcaad0f894e7b468486ddd5cbdfe82af4d41
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-01-30 15:21:52 +0100

    Implement service collection extensions

---
+++ src/DiscordTemplateBot.Commands/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using DiscordTemplateBot.Commands.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordTemplateBot.Commands.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDiscordCommands(this IServiceCollection services, LogSeverity logSeverity)
+    {
+        services.AddDiscordInteractionService();
+        services.AddDiscordCommandService(logSeverity);
+        services.AddHostedService<InteractionHandlingService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDiscordInteractionService(this IServiceCollection services)
+    {
+        services.AddSingleton(opt =>
+        {
+            var client = opt.GetRequiredService<DiscordSocketClient>();
+            return new InteractionService(client);
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddDiscordCommandService(this IServiceCollection services,
+        LogSeverity logSeverity)
+    {
+        var config = new CommandServiceConfig
+        {
+            DefaultRunMode = Discord.Commands.RunMode.Async,
+            LogLevel = logSeverity
+        };
+        services.AddSingleton(_ => new CommandService(config));
+
+        return services;
+    }
+}
