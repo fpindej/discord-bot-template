ba1afca7fca098a82ecee479c11861a6a31e628f
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-06-02 20:46:29 +0200

    Move severity logging from Logging to bot configuration

--- src/Discord.BotConfiguration/Extensions/LoggerExtensions.cs
+++ src/Discord.BotConfiguration/Extensions/LoggerExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
 
 namespace Discord.BotConfiguration.Extensions;
 
@@ -8,5 +10,26 @@ public static class LoggerExtensions
     {
         return configuration.GetSection(BotConfiguration.SectionName)
             .Get<BotConfiguration>()!.LogLevel;
+    }
+    
+    /// <summary>
+    ///     Maps Discord.NET's LogSeverity to Serilog's LogEventLevel
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public static Task LogAsync(LogMessage message)
+    {
+        var severity = message.Severity switch
+        {
+            LogSeverity.Critical => LogEventLevel.Fatal,
+            LogSeverity.Error => LogEventLevel.Error,
+            LogSeverity.Warning => LogEventLevel.Warning,
+            LogSeverity.Info => LogEventLevel.Information,
+            LogSeverity.Verbose => LogEventLevel.Verbose,
+            LogSeverity.Debug => LogEventLevel.Debug,
+            _ => LogEventLevel.Information
+        };
+        Log.Write(severity, message.Exception, "[{Source}] {Message}", message.Source, message.Message);
+        return Task.CompletedTask;
     }
 }
